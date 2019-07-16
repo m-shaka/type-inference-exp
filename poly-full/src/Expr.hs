48f9cae2c8bc942ef2539ac06f5a8b15bccea291
m-shaka <buddha0818@gmail.com>
2019-07-17 00:58:03 +0900

    solve deps

--- poly-full/src/Expr.hs
+++ poly-full/src/Expr.hs
@@ -1,16 +1,6 @@
 module Expr where
 
-import           Control.Monad.Catch
-import           Control.Monad.Except
-import           Data.Foldable
-import           Data.Functor.Identity (runIdentity)
-import           Data.List             ((\\))
-import qualified Data.Map              as Map
-import           Data.Maybe            (fromJust)
-
 type Name = String
-
-type Index = Int
 
 data Expr =
   Var Name
@@ -26,41 +16,9 @@ data BinOp =
   | Multi
   deriving (Eq, Show)
 
-type Bind = (Name, Expr)
-
-type BindGroup = [[Bind]]
-
-newtype BindError =
-  UndefinedVar Name
-  deriving(Eq, Show)
-
-instance Exception BindError
-
-type WithBindError = Except BindError
-
 fvars :: Expr -> [Name]
 fvars (Var n)         = [n]
 fvars (Lam x e)       = [n | n <- fvars e, n /= x]
 fvars (App e1 e2)     = fvars e1 ++ fvars e2
 fvars (BinOp _ e1 e2) = fvars e1 ++ fvars e2
 fvars _               = []
-
-findDeps :: [Bind] -> [Name] -> WithBindError [(Index, Index)]
-findDeps binds tenvNames = foldrM foldFunc [] binds
-  where
-    bindNames = fmap fst binds
-    nameToIndex = Map.fromList $ zip bindNames [0..]
-    lookupFv :: Index -> Name -> WithBindError (Index, Index)
-    lookupFv i varName = case Map.lookup varName nameToIndex of
-      Just i' -> pure (i, i')
-      _       -> throwError $ UndefinedVar varName
-    find (name, expr) = do
-      let fv = fvars expr \\ (bindNames ++ tenvNames)
-          srcIndex = fromJust $ Map.lookup name nameToIndex
-      traverse (lookupFv srcIndex) fv
-    foldFunc bind acc = do
-      indices <- find bind
-      pure $ indices ++ acc
-
-run :: [Bind] -> [Name] -> Either BindError [(Index, Index)]
-run binds tenvNames = runIdentity . runExceptT $ findDeps binds tenvNames
--- poly-full/test/Spec.hs
+++ poly-full/test/Spec.hs
@@ -1,4 +1,20 @@
 import           Test.Hspec
 
+import qualified BindGroup  as BG
+import           Expr
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "bindGroup" $ do
+    it "mutual recursion" $ do
+      let n = Int 0
+          gx = App (Var "g") (Var "x")
+          f = Lam "x" $ BinOp Multi (Int 2) gx
+          yPlus1 = BinOp Plus (Var "y") (Int 1)
+          g = Lam "y" $ App (Var "f") yPlus1
+          h =  App (Var "f") (Int 1)
+          binds = [("n", n), ("f", f), ("g", g), ("h", h)]
+          env = []
+          Right bg = BG.run binds env
+      bg `shouldBe` [[("f", f), ("g", g)], [("h", h)], [("n", n)]]
+
