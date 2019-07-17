2d5d49ef379d9f5e3257c1f556b79639feca8a8f
m-shaka <buddha0818@gmail.com>
2019-07-17 13:15:17 +0900

    add expression test

--- poly-full/test/Spec.hs
+++ poly-full/test/Spec.hs
@@ -4,6 +4,7 @@ import qualified BindGroup           as BG
 import           Control.Monad.State
 import           Expr
 import           Infer
+import           Subst
 import           Type
 
 main :: IO ()
@@ -17,14 +18,42 @@ main = hspec $ do
       binds = [("n", n), ("f", f), ("g", g), ("h", h)]
       env = []
 
-  describe "bindGroup" $
+  describe "infer expression" $ do
+    let inferExpr' env expr =
+          (\(t, (_, s)) -> apply s t) <$> runStateT (inferExpr env expr) (0, [])
+        env =
+          [("x", toScheme tBool)
+          , ("toInt", toScheme $ tFun tBool tInt)
+          , ("id", Forall 1 $ tFun (TGen 0) (TGen 0))]
+
+    it "var" $ do
+      let Right t = inferExpr' env (Var "x")
+      t `shouldBe` tBool
+    it "int" $ do
+      let Right t = inferExpr' env (Int 0)
+      t `shouldBe` tInt
+    it "bool" $ do
+      let Right t = inferExpr' env (Bool True)
+      t `shouldBe` tBool
+    it "lambda" $ do
+      let Right t = inferExpr' env (Lam "x" (App (Var "toInt") (Var "x")))
+      t `shouldBe` tFun tBool tInt
+    it "app" $ do
+      let Right t = inferExpr' env (App (Var "toInt") (Bool False))
+      t `shouldBe` tInt
+    it "poly" $ do
+      let expr = App (App (Var "id") (Var "toInt")) (App (Var "id") (Var "x"))
+          Right t = inferExpr' env expr
+      t `shouldBe` tInt
+
+  describe "build bindGroup" $
     it "mutual recursion" $ do
       let Right bg = BG.run binds env
       bg `shouldBe` [[("f", f), ("g", g)], [("h", h)], [("n", n)]]
 
-  describe "infer" $
+  describe "infer bingGroup" $
     it "mutual recursion" $ do
       let Right bg = BG.run binds env
           Right env' = infer env bg
-      let int2int = tFun (TCon "Int") (TCon "Int")
+          int2int = tFun (TCon "Int") (TCon "Int")
       env' `shouldBe` [("n",Forall 0 (TCon "Int")),("h",Forall 0 (TCon "Int")),("f",Forall 0 int2int),("g",Forall 0 int2int)]
